6cd0cf577f83c93c6afa294d205f4a02c87670c8
litsynp <nocte55is@gmail.com>
2023-05-23 23:29:41 +0900

    feat: add tracing

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,13 @@
 use std::net::SocketAddr;
 
+use tracing::info;
+
 use crate::infrastructure::database;
 use crate::routes;
 
 pub async fn create_server() {
+    tracing_subscriber::fmt::init();
+
     let db_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     let pool = database::get_postgres_pool(db_url.as_str())
@@ -20,7 +24,7 @@ pub async fn create_server() {
     let router = routes::build_routes(pool);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    println!("Listening on {}", addr);
+    info!("Listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(router.into_make_service())
