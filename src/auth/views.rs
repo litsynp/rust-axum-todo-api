7ad69827798ba0281d69e828064f3dd756f5f18d
litsynp <nocte55is@gmail.com>
2024-03-29 19:23:34 +0900

    chore: camel case views

--- src/auth/views.rs
+++ src/auth/views.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
 #[derive(Deserialize, Serialize, ToSchema, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct LoginRequest {
     #[schema(example = "user@example.com")]
     pub email: String,
@@ -9,6 +10,7 @@ pub struct LoginRequest {
 }
 
 #[derive(Deserialize, Serialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
 pub struct TokenView {
     pub user_id: i32,
     pub access_token: String,
